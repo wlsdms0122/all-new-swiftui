3dcbdac7661e86cfb43f71f3e05bb7974e20c998
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-06-06 23:24:59 +0900

    SwiftUI Essentials - Handling User Input
    Section 4 - Use a Bindable Object for Storage

---
+++ Handling User Input/Handling User Input/Models/UserData.swift
@@ -0,0 +1,26 @@
+//
+//  UserData.swift
+//  Handling User Input
+//
+//  Created by JSilver on 2019/06/06.
+//  Copyright © 2019 JSilver. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: BindableObject {
+    let didChange = PassthroughSubject<UserData, Never>()
+    
+    var showFavoritesOnly = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    var landmarks = landmarkData {
+        didSet {
+            didChange.send(self)
+        }
+    }
+}
