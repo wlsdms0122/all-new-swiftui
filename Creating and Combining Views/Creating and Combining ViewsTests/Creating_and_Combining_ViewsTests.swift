16e67cf7a97eef23200f83af0ade8af9d65a5655
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-06-06 12:47:41 +0900

    SwiftUI Essentials - Creating and Combining Views
    Section 1 - Create a New Project and Explore the Canvas

---
+++ Creating and Combining Views/Creating and Combining ViewsTests/Creating_and_Combining_ViewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Creating_and_Combining_ViewsTests.swift
+//  Creating and Combining ViewsTests
+//
+//  Created by JSilver on 2019/06/06.
+//  Copyright © 2019 JSilver. All rights reserved.
+//
+
+import XCTest
+@testable import Creating_and_Combining_Views
+
+class Creating_and_Combining_ViewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
