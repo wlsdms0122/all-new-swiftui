1adf117ba55f5616c4399f6bbcdc183eeb4d711d
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-06-06 23:20:32 +0900

    SwiftUI Essentials - Handling User Input
    Section 2 - Filter the List View

--- Handling User Input/Handling User Input/LandmarkList.swift
+++ Handling User Input/Handling User Input/LandmarkList.swift
@@ -8,11 +8,15 @@ A view showing a list of landmarks.
 import SwiftUI
 
 struct LandmarkList: View {
+    @State var showFavoritesOnly = false
+    
     var body: some View {
         NavigationView {
             List(landmarkData) { landmark in
-                NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
+                if !self.showFavoritesOnly || landmark.isFavorite {
+                    NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationBarTitle(Text("Landmarks"), displayMode: .large)
@@ -23,11 +27,7 @@ struct LandmarkList: View {
 #if DEBUG
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"].identified(by: \.self)) { deviceName in
             LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
     }
 }
 #endif
