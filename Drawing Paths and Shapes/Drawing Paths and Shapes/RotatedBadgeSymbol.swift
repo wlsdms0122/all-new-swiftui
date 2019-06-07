8fcc02cee604129cea729866f81abd1ca247be8a
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-06-07 20:32:00 +0900

    Drawing and Animation - Drawing Paths and Shapes
    Section 4 - Combine the Badge Foreground and Background

---
+++ Drawing Paths and Shapes/Drawing Paths and Shapes/RotatedBadgeSymbol.swift
@@ -0,0 +1,27 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Drawing Paths and Shapes
+//
+//  Created by JSilver on 2019/06/07.
+//  Copyright © 2019 JSilver. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol : View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#if DEBUG
+struct RotatedBadgeSymbol_Previews : PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
+#endif
