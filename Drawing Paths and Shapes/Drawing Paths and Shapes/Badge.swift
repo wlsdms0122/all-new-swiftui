c7048900ddea6ca128a680e05123fa5e647d31c2
Jeong Jin Eun <wlsdms0122@gmail.com>
2019-06-07 19:49:29 +0900

    Drawing and Animation - Drawing Paths and Shapes
    Section 1 - Create a Badge View

---
+++ Drawing Paths and Shapes/Drawing Paths and Shapes/Badge.swift
@@ -0,0 +1,23 @@
+//
+//  Badge.swift
+//  Drawing Paths and Shapes
+//
+//  Created by JSilver on 2019/06/07.
+//  Copyright © 2019 JSilver. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge : View {
+    var body: some View {
+        Text("Badge")
+    }
+}
+
+#if DEBUG
+struct Badge_Previews : PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
+#endif
---
+++ Drawing Paths and Shapes/Drawing Paths and ShapesTests/Drawing_Paths_and_ShapesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Drawing_Paths_and_ShapesTests.swift
+//  Drawing Paths and ShapesTests
+//
+//  Created by JSilver on 2019/06/07.
+//  Copyright © 2019 JSilver. All rights reserved.
+//
+
+import XCTest
+@testable import Drawing_Paths_and_Shapes
+
+class Drawing_Paths_and_ShapesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
